e4811ec318d7dd45d39def1042a0e3ea04a576a6
Mayu Imanaka <imamayu.1215@gmail.com>
2020-01-07 09:41:20 -0800

    add:redstarter as inhabitent

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -1,58 +1,35 @@
 <?php
-
 /**
- * The header for our theme
+ * The header for our theme.
  *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package inhabitent
+ * @package RED_Starter_Theme
  */
 
-?>
-<!doctype html>
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
-<head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
-</head>
+	</head>
 
-<body <?php body_class(); ?>>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'inhabitent'); ?></a>
+	<body <?php body_class(); ?>>
+		<div id="page" class="hfeed site">
+			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
-		<header id="masthead" class="site-header header">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if (is_front_page() && is_home()) :
-				?>
-					<div class="header__nav">
+			<header id="masthead" class="site-header" role="banner">
+				<div class="site-branding">
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+				</div><!-- .site-branding -->
 
-					</div>
-				<?php
-				endif;
-				$inhabitent_description = get_bloginfo('description', 'display');
-				if ($inhabitent_description || is_customize_preview()) :
-				?>
-					<p class="site-description"><?php echo $inhabitent_description; /* WPCS: xss ok. */ ?></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</header><!-- #masthead -->
 
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'inhabitent'); ?></button>
-				<?php
-				wp_nav_menu(array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				));
-				?>
-			</nav><!-- #site-navigation -->
-		</header><!-- #masthead -->
-
-		<div id="content" class="site-content">
+			<div id="content" class="site-content">
--- themes/inhabitent/template-parts/content-search.php
+++ themes/inhabitent/template-parts/content-search.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * Template part for displaying results in search pages
+ * Template part for displaying results in search pages.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package inhabitent
+ * @package RED_Starter_Theme
  */
 
 ?>
@@ -15,21 +13,12 @@
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php
-			inhabitent_posted_on();
-			inhabitent_posted_by();
-			?>
+			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php inhabitent_post_thumbnail(); ?>
-
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php inhabitent_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+</article><!-- #post-## -->
