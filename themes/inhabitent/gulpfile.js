7adf6634647f416da4b26564713bd2598cba0357
Mayu Imanaka <imamayu.1215@gmail.com>
2020-01-07 18:27:24 -0800

    add:gulp

--- themes/inhabitent/gulpfile.js
+++ themes/inhabitent/gulpfile.js
@@ -12,7 +12,7 @@ const babel = require('gulp-babel');
 
 // Create basic Gulp tasks
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp
     .src('./sass/style.scss', { sourcemaps: true })
     .pipe(sourcemaps.init())
@@ -30,7 +30,7 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp
     .src(['./js/*.js'])
     .pipe(prettyError())
@@ -61,7 +61,7 @@ gulp.task(
 
 // Set-up BrowserSync and watch
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   const files = [
     './build/css/*.css',
     './build/js/*.js',
@@ -70,13 +70,13 @@ gulp.task('browser-sync', function() {
   ];
 
   browserSync.init(files, {
-    proxy: 'localhost[:port-here]/[your-dir-name-here]'
+    proxy: 'localhost:3000/inhabitent'
   });
 
   gulp.watch(files).on('change', browserSync.reload);
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('js/*.js', gulp.series('scripts'));
   gulp.watch('sass/*.scss', gulp.series('sass'));
 });
