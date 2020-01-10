4450d6934403d753be1d808c7c96113ad1b7ba6b
Mayu Imanaka <imamayu.1215@gmail.com>
2020-01-09 18:54:26 -0800

    add:d

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer.
  *
@@ -7,16 +8,42 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+<footer class="footer" role="contentinfo">
+	<div class="footer__box">
+		<div class="footer__info">
+			<div class="footer__list footer__list--contact">
+				<h3>CONTACT INFO</h3>
+				<ul class="footer__infoNav">
+					<li><i class="far fa-envelope"></i>info@inhabitent.com</li>
+					<li><i class="fas fa-phone-alt"></i>778-456-7891</li>
+				</ul>
+				<div class="footer__infoSocial">
+					<a href=""><i class="fab fa-facebook-square"></i></a>
+					<a href=""><i class="fab fa-twitter-square"></i></a>
+					<a href=""><i class="fab fa-google-plus-square"></i></a>
+				</div>
+			</div>
+			<div class="footer__list footer__list--hours">
+				<h3>BUSINESS HOURS</h3>
+				<ul class="footer__infoNav">
+					<li><b>Monday-Friday</b>:9am to 5pm</li>
+					<li><b>Saturday</b>:10am to 2pm</li>
+					<li><b>Sunday</b>:Closed</li>
+				</ul>
+			</div>
+		</div>
+		<div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-text.svg" alt=""></div>
+	</div>
+	<div class="footer__copy">COPYRIGHT © 2020 INHABITENT</div>
+</footer>
+</div>
+</div>
+<script src="https://kit.fontawesome.com/be014b79a7.js" crossorigin="anonymous"></script>
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-	</body>
+</body>
+
 </html>
