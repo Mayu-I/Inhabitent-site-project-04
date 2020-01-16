d2e6ec4effb565dc034c624d8bc928c23ad5f1c9
Mayu Imanaka <imamayu.1215@gmail.com>
2020-01-15 21:02:43 -0800

    update:header transition

--- themes/inhabitent/js/header.js
+++ themes/inhabitent/js/header.js
@@ -1,19 +1,18 @@
 $(function() {
   let $body = $('body');
   let $nav = $('.header');
-  let heroHeight = $('.home-hero').offset().top;
-  let adHeight = $('.wp-post-image').offset().top;
+//   let adHeight = $('.wp-post-image').offset().top;
   $(window).on("scroll", function() {
+    //   console.log(adHeight);
     var scroll = $(window).scrollTop() + $(window).height();
     if($body.hasClass('home') || $body.hasClass('page-about')){       
-        // console.log(navHeight) ;
-    if(scroll >= heroHeight) {
+    if(scroll >= 1365) {
       $nav.removeClass('header--white');
     }else{
       $nav.addClass('header--white');
     }
     }else if($body.hasClass('single-adventures')){
-        if(scroll >= adHeight) {
+        if(scroll >= 1650) {
       $nav.removeClass('header--white');
     }else{
       $nav.addClass('header--white');
