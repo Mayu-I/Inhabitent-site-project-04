1dc02a1fab19629ab26010f41dc9210917ba343e
Mayu Imanaka <imamayu.1215@gmail.com>
2020-01-15 20:41:25 -0800

    add:header scroll transition

--- themes/inhabitent/js/header.js
+++ themes/inhabitent/js/header.js
@@ -1,12 +1,23 @@
 $(function() {
+  let $body = $('body');
   let $nav = $('.header');
-  let navHeight = $('.home-hero').offset().top;
+  let heroHeight = $('.home-hero').offset().top;
+  let adHeight = $('.wp-post-image').offset().top;
   $(window).on("scroll", function() {
     var scroll = $(window).scrollTop() + $(window).height();
-    if(scroll >= navHeight) {
+    if($body.hasClass('home') || $body.hasClass('page-about')){       
+        // console.log(navHeight) ;
+    if(scroll >= heroHeight) {
       $nav.removeClass('header--white');
     }else{
       $nav.addClass('header--white');
+    }
+    }else if($body.hasClass('single-adventures')){
+        if(scroll >= adHeight) {
+      $nav.removeClass('header--white');
+    }else{
+      $nav.addClass('header--white');
+    }
     }
   });
 });
