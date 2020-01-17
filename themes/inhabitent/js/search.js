8d6746af0642130c2e08b29daea16eeb19692098
Mayu Imanaka <imamayu.1215@gmail.com>
2020-01-16 21:21:03 -0800

    add:a toggle-able search

--- themes/inhabitent/js/search.js
+++ themes/inhabitent/js/search.js
@@ -1,5 +1,11 @@
 $(function () {
     $('.search-toggle').click(function () {
         $('.search-label').stop(true).animate({ width: 'toggle' }, 'slow');
+        $('.search-field').focus();
     });
+    $('.search-field').blur(function() {
+    if ($(this).val() === '') {
+            $('.search-label').stop(true).animate({ width: 'toggle' }, 'slow');
+     }
+     });
 });
